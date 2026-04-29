c8c96575b8cb67d588dd6208948ba8d86fb81e8c
isa18266 <81499951+isa18266@users.noreply.github.com>
2026-04-28 23:52:48 -0500

    Update servicio.py

--- servicio.py
+++ servicio.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
 
+# CLASE ABSTRACTA "isabel"
 class Servicio(ABC):
     def __init__(self, nombre, precio_base):
         self.nombre = nombre
@@ -12,3 +13,30 @@ class Servicio(ABC):
     @abstractmethod
     def descripcion(self):
         pass
+
+
+# CLASE 1: Reserva de Sala "isabel"
+class ReservaSala(Servicio):
+    def calcular_costo(self, duracion):
+        return self.precio_base * duracion
+
+    def descripcion(self):
+        return f"Reserva de sala por {self.precio_base} por hora"
+
+
+# CLASE 2: Alquiler de Equipos "isabel"
+class AlquilerEquipo(Servicio):
+    def calcular_costo(self, duracion):
+        return (self.precio_base * duracion) + 20
+
+    def descripcion(self):
+        return "Alquiler de equipos con costo adicional"
+
+
+# CLASE 3: Asesoría "isabel"
+class Asesoria(Servicio):
+    def calcular_costo(self, duracion):
+        return self.precio_base * duracion * 1.2
+
+    def descripcion(self):
+        return "Asesoría especializada"
